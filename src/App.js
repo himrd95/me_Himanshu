0b0b158bb3efca28e99dd4a984c284ee32b339bb
Himanshu <himrd95@gmail.com>
2021-08-11 14:14:45 +0530

    Formatted document

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ function App() {
 		React.useContext(ThemeContext);
 	const scrollRef = useRef();
 
-	useEffect(()=>{
-		setTimeout(()=>{
-			setState(true)
-		}, 2000)
-	}, [])
+	useEffect(() => {
+		setTimeout(() => {
+			setState(true);
+		}, 2000);
+	}, []);
 	// const handleTop = () => {
 	// 	console.log('clicked', scrollRef);
 	// 	scrollRef.current.scrollTop = 0;
@@ -22,38 +22,47 @@ function App() {
 
 	return (
 		<React.Fragment>
-			{!state ? <div
-		style={{
-			background: `${newTheme.background}`,
-		}} className="logoStart">
-					<img src="https://github.com/himrd95/portfolio/blob/main/src/assets/logo_new.png?raw=true" alt="logo"/>
-				</div> : <div className="components">
+			{!state ? (
 				<div
 					style={{
-						color: `${newTheme.title}`,
-						left: `${open ? "-100vw" : "0"}`,
+						background: `${newTheme.background}`,
 					}}
-					className="links"
+					className="logoStart"
 				>
-					<a onClick={handleMenu} href="#home">
-						Home
-					</a>
-					<a onClick={handleMenu} href="#about">
-						About
-					</a>
-					<a onClick={handleMenu} href="#techStacks">
-						Profeciencies
-					</a>
-					<a onClick={handleMenu} href="#projects">
-						Projects
-					</a>
-					<a onClick={handleMenu} href="#contact">
-						Contact
-					</a>
+					<img
+						src="https://github.com/himrd95/portfolio/blob/main/src/assets/logo_new.png?raw=true"
+						alt="logo"
+					/>
 				</div>
-				<Navbar />
-				<Home scrollRef={scrollRef} />
-			</div>}
+			) : (
+				<div className="components">
+					<div
+						style={{
+							color: `${newTheme.title}`,
+							left: `${open ? "-100vw" : "0"}`,
+						}}
+						className="links"
+					>
+						<a onClick={handleMenu} href="#home">
+							Home
+						</a>
+						<a onClick={handleMenu} href="#about">
+							About
+						</a>
+						<a onClick={handleMenu} href="#techStacks">
+							Profeciencies
+						</a>
+						<a onClick={handleMenu} href="#projects">
+							Projects
+						</a>
+						<a onClick={handleMenu} href="#contact">
+							Contact
+						</a>
+					</div>
+					<Navbar />
+					<Home scrollRef={scrollRef} />
+				</div>
+			)}
 		</React.Fragment>
 	);
 }
