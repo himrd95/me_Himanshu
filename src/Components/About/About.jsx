41abdd1c5ca4112838a2f14c1e7d21f4db2a1016
Himanshu <himrd95@gmail.com>
2021-08-21 19:11:11 +0530

    done few changes

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,26 +1,26 @@
-import React from "react";
-import { ThemeContext } from "../../ContextProvider/ThemeContext";
-import WindowSize from "../../Utils/WindowSize";
-import styles from "./About.module.css";
+import React from 'react';
+import { ThemeContext } from '../../ContextProvider/ThemeContext';
+import WindowSize from '../../Utils/WindowSize';
+import styles from './About.module.css';
 
 const About = () => {
 	const { newTheme } = React.useContext(ThemeContext);
 	const [width] = WindowSize();
 	const img =
 		width < 650
-			? "https://github.com/himrd95/portfolio/blob/main/src/assets/profile.png?raw=true"
-			: "https://github.com/himrd95/portfolio/blob/main/public/developer.gif?raw=true";
+			? 'https://github.com/himrd95/me_Himanshu/blob/main/src/assets/profile_pic_without_background.png?raw=true'
+			: 'https://github.com/himrd95/portfolio/blob/main/public/developer.gif?raw=true';
 	return (
 		<div
-			data-aos="fade-down"
-			data-aos-offset="200"
-			data-aos-easing="ease-in-sine"
-			data-aos-duration="800"
+			data-aos='fade-down'
+			data-aos-offset='200'
+			data-aos-easing='ease-in-sine'
+			data-aos-duration='800'
 			className={styles.container}
 			style={{ boxShadow: `3px 3px 5px ${newTheme.line}` }}
 		>
 			<div className={styles.first}>
-				<img src={img} alt="" />
+				<img src={img} alt='' />
 			</div>
 			<div className={styles.second}>
 				<h1
@@ -43,7 +43,7 @@ const About = () => {
 					and communication. After months of rigorous training, here I
 					am
 					<span style={{ color: `#00a0a0` }}>
-						{" "}
+						{' '}
 						looking for an opportunity as a full stack web developer
 					</span>
 					.
